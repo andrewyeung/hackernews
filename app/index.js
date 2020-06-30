04a6de5e29030404726d7ab4c25940b482025c45
andrewyeung <andrewyeung121@gmail.com>
2020-06-29 22:32:37 -0700

    nav reload components

--- app/index.js
+++ app/index.js
@@ -11,8 +11,8 @@ class App extends React.Component {
     <Router>
       <div>
         <div className="wrapper">
-          <Route exact path='/' component={() => {<Top timestamp={new Date().toString()} />}} />
-          <Route exact path='/new' component={() => {<New timestamp={new Date().toString()} />}} />
+          <Route exact path='/' render={() => {<Top timestamp={new Date().toString()} />}} />
+          <Route exact path='/new' render={() => {<New timestamp={new Date().toString()} />}} />
         </div>
       </div>
     </Router>
